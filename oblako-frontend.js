35a82d67d2bc83d5b2a2d0bde145911b821fb69f
Doomsdhg <tohanknv@yandex.ru>
2022-08-29 16:04:55 +0300

    FRNT-1: server paths fix

--- oblako-frontend.js
+++ oblako-frontend.js
@@ -7,8 +7,8 @@ function requireHTTPS(req, res, next) {
 const express = require("express");
 const app = express();
 app.use(requireHTTPS);
-app.use(express.static("./dist/oblako-frontend.json"));
+app.use(express.static("./dist/oblako-frontend"));
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/oblako-frontend.json/" });
+  res.sendFile("index.html", { root: "dist/oblako-frontend/" });
 });
 app.listen(process.env.PORT || 8080);
